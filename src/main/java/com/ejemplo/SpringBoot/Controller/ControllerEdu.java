aabf9570d5857583d53b50b441df920ad2358793
loboxos <vcristian528@gmail.com>
2022-08-26 01:46:03 -0300

    reparandoErroresback

--- src/main/java/com/ejemplo/SpringBoot/Controller/ControllerEdu.java
+++ src/main/java/com/ejemplo/SpringBoot/Controller/ControllerEdu.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = "https:frontendprueba0912.web.app")
+@CrossOrigin(origins = "https://frontendprueba0912.web.app")
 public class ControllerEdu {
     @Autowired
     EducacionService sEducacion;
