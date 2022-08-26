c8785e4965bc8e60b71d65c5898cd535f48e4eb5
loboxos <vcristian528@gmail.com>
2022-08-26 14:48:11 -0300

    hsaj

--- src/main/java/com/ejemplo/SpringBoot/Controller/Controller.java
+++ src/main/java/com/ejemplo/SpringBoot/Controller/Controller.java
@@ -3,7 +3,6 @@ package com.ejemplo.SpringBoot.Controller;
 
 import com.ejemplo.SpringBoot.model.Persona;
 import com.ejemplo.SpringBoot.service.IPersonaService;
-//import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -24,26 +23,22 @@ public class Controller {
   
     @Autowired
     private IPersonaService persoServ;
-  
     @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/new/persona")
     public void agregarPersona(@RequestBody Persona pers){
-      persoServ.crearPersona(pers);
-        // listaPersonas.add(pers);        
+      persoServ.crearPersona(pers);       
     }
-    
     @GetMapping("/ver/personas")
     @ResponseBody
     public List<Persona>verPersonas(){
       return persoServ.verPersonas();
-        // return listaPersonas;
     }
-      @PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/delete/{id}")
     public void borrarPersona(@PathVariable Long id){
         persoServ.borrarPersona(id);
     }
-      @PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/personas/editar/{id}")
     public Persona editPersona(@PathVariable Long id,
             @RequestParam("nombre") String nuevoNombre,
@@ -56,7 +51,6 @@ public class Controller {
         persoServ.crearPersona(persona);
         return persona;
     }
-    
     @GetMapping("/personas/traer/perfil")
     public Persona findPersona(){
         return persoServ.buscarPersona((long)4);
