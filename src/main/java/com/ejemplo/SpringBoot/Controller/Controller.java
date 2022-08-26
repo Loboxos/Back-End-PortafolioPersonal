2cfbf1a899c2e5ac3be2ae45fe06bb832660c050
loboxos <vcristian528@gmail.com>
2022-08-26 12:36:09 -0300

    cambios

--- src/main/java/com/ejemplo/SpringBoot/Controller/Controller.java
+++ src/main/java/com/ejemplo/SpringBoot/Controller/Controller.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "https://frontendprueba0912.web.app")
+@CrossOrigin(origins = "+")
 public class Controller {
   
     @Autowired
